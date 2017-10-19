8dc0298a7c9e83dbc3fd3f0df858b93b11826b33
Juan Gomez <j.gomez@drexed.com>
2017-10-18 22:49:31 -0400

    clean up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,57 +1,11 @@
 # frozen_string_literal: true
 
-require 'trumail'
-
-module LetManager
-  extend RSpec::SharedContext
-
-  let(:json_data) do
-    <<-DATA
-      {
-        "address":"test@gmail.com",
-        "username":"test",
-        "domain":"gmail.com",
-        "hostExists":true,
-        "deliverable":false,
-        "fullInbox":false,
-        "catchAll":false,
-        "disposable":false,
-        "gravatar":true
-      }
-    DATA
-  end
-
-  let(:xml_data) do
-    <<-DATA
-      <lookup>
-        <address>test@gmail.com</address>
-        <username>test</username>
-        <domain>gmail.com</domain>
-        <hostExists>true</hostExists>
-        <deliverable>false</deliverable>
-        <fullInbox>false</fullInbox>
-        <catchAll>false</catchAll>
-        <disposable>false</disposable>
-        <gravatar>true</gravatar>
-      </lookup>
-    DATA
-  end
-
-  let(:response_hash) do
-    {
-      'address' => 'test@gmail.com',
-      'username' => 'test',
-      'domain' => 'gmail.com',
-      'hostExists' => true,
-      'deliverable' => false,
-      'fullInbox' => false,
-      'catchAll' => false,
-      'disposable' => false,
-      'gravatar' => true
-    }
-  end
-
+%w[trumail pathname].each do |file_name|
+  require file_name
 end
+
+spec_support_path = Pathname.new(File.expand_path('../spec/support', File.dirname(__FILE__)))
+load(spec_support_path.join('rspec/let_manager.rb'))
 
 RSpec.configure do |config|
   config.include LetManager
---
+++ spec/support/rspec/let_manager.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+module LetManager
+  extend RSpec::SharedContext
+
+  let(:json_data) do
+    <<-DATA
+      {
+        "address":"test@gmail.com",
+        "username":"test",
+        "domain":"gmail.com",
+        "hostExists":true,
+        "deliverable":false,
+        "fullInbox":false,
+        "catchAll":false,
+        "disposable":false,
+        "gravatar":true
+      }
+    DATA
+  end
+
+  let(:xml_data) do
+    <<-DATA
+      <lookup>
+        <address>test@gmail.com</address>
+        <username>test</username>
+        <domain>gmail.com</domain>
+        <hostExists>true</hostExists>
+        <deliverable>false</deliverable>
+        <fullInbox>false</fullInbox>
+        <catchAll>false</catchAll>
+        <disposable>false</disposable>
+        <gravatar>true</gravatar>
+      </lookup>
+    DATA
+  end
+
+  let(:response_hash) do
+    {
+      'address' => 'test@gmail.com',
+      'username' => 'test',
+      'domain' => 'gmail.com',
+      'hostExists' => true,
+      'deliverable' => false,
+      'fullInbox' => false,
+      'catchAll' => false,
+      'disposable' => false,
+      'gravatar' => true
+    }
+  end
+
+end
