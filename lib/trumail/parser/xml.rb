49d3a340727c21842a112ccda167ece5e68ee13a
Juan Gomez <j.gomez@drexed.com>
2020-11-07 00:15:16 -0500

    Linter fixes

--- lib/trumail/parser/xml.rb
+++ lib/trumail/parser/xml.rb
@@ -6,19 +6,19 @@ module Trumail
   module Parser
     class Xml < Trumail::Parser::Base
 
-      # rubocop:disable Security/Eval
+      # rubocop:disable Security/Eval, Style/SingleArgumentDig
       def parse
         @hash = Ox.load(@data, mode: :hash_no_attrs)
                   .dig(:lookup)
                   .each_with_object({}) do |(key, val), hash|
                     hash[key.to_s] = begin
-                                       eval(val)
-                                     rescue StandardError, SyntaxError
-                                       val
-                                     end
+                      eval(val)
+                    rescue StandardError, SyntaxError
+                      val
+                    end
                   end
       end
-      # rubocop:enable Security/Eval
+      # rubocop:enable Security/Eval, Style/SingleArgumentDig
 
     end
   end
